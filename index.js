a94d0414ba62daed634e2427b3c8f529811dd320
Your Name <you@example.com>
2023-05-27 02:36:51 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+
+// let day = sunday;
+
+// switch(day){
+//     case "Monday":
+//     alert("today is Monday")
+//     break;
+
+//     case "Tuesday":
+//     alert("today is Tuesday")
+//     break;
+
+//     case "Wednesday":
+//     alert("today is Wednesday")
+//     break;
+
+//     case "Thursday":
+//     alert("today is Thursday")
+//     break;
+
+//     case "Friday":
+//     alert("today is Friday")
+//     break;
+
+//     case "Saturday":
+//     alert("today is Saturday")
+//     break;
+
+//     case "Sunday":
+//     alert("today is Sunday")
+//     break;
+
+// }
+
+
+let i=0
+
+while(i<10){
+    i++
+    console.log(i)
+}
+ let a=0;
+do{
+    a++
+    console.log(a)
+}while(a<10)
